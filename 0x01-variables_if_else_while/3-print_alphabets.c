e8fb20bd6465a73d5239901079d398cae6cbd76b
Walesketch <nurudeenolawale3@gmail.com>
2022-09-12 17:06:37 -0700

    Upper

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main block
+ * descrption:using 'putchar'
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+} 
