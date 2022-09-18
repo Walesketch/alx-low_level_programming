3b62cdfdab3239fb0bc4064ec09d7c6b0be96aeb
Walesketch <nurudeenolawale3@gmail.com>
2022-09-17 21:12:55 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 /**
- * main: main block
- * description: Use `putchar` to print lowercase andthen uppercase alphabet.
+ * main - main block
+ * Description: Use `putchar` to print lowercase andthen uppercase alphabet.
  * Return: 0
  */
 int main(void)
 {
 char c = 'a';
-while(c <= 'z')
+
+while (c <= 'z')
 {
 putchar(c);
 c++;
 }
+
+c = 'A';
+
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+
 putchar('\n');
+
 return (0);
 }
 
